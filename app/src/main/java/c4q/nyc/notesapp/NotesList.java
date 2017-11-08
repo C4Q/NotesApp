ee31c70a447805dd48efbeadc7ddcfcdcdf33098
Justice Ogbonna <ebesco@yahoo.com>
2017-11-07 21:59:24 -0500

    added menu and updated note detail view

--- app/src/main/java/c4q/nyc/notesapp/NotesList.java
+++ app/src/main/java/c4q/nyc/notesapp/NotesList.java
@@ -1,7 +1,11 @@
 package c4q.nyc.notesapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class NotesList extends AppCompatActivity {
 
@@ -10,6 +14,24 @@ public class NotesList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes_list);
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.noteslist_menu, menu);
+      return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.add_note_button:
+                Intent intent = new Intent(this, NoteDetail.class);
+                startActivity(intent);
+                break;
+        }
+        return true;
+    }
+
 
     public void launchViewNote() {
         // launches note detail activity in view mode
